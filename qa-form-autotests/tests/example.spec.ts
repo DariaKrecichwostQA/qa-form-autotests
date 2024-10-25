0894c888309a1a09876454acd20b4313783f6d34
Daria <clever.it.office@gmail.com>
2024-10-25 16:00:32 +0200

    title fix

--- qa-form-autotests/tests/example.spec.ts
+++
@@ -1,9 +0,0 @@
-import { test } from '../page-object-model';
-
-test('check', async ({ page, registrationPage }) => {
-  await page.goto('http://localhost:8080');
-  console.log(registrationPage.name);
-});
-
-
-
